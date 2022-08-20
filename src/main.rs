fda2ec0e32644a2f4092a75ce1f32db447e41245
heru <heru.ekos@gmail.com>
2022-08-20 13:58:20 +0700

    initial project

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().json("hello from mongodb")
+}
+
+#[actix_web::main]
+async fn main() std::io::Result<()> {
+    HttpServer::new(|| App::new().service(hello))
+        .bind(("localhost", 8080))?
+        .run()
+        .await
+}
