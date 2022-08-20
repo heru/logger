583ff9a3b6dabfaf349ac2d90faf5b189217660d
heru <heru.ekos@gmail.com>
2022-08-20 14:38:10 +0700

    test post user

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+mod api;
+mod models;
+mod repository;
+
+use actix_web::{web::Data,  App, HttpServer, HttpResponse, Responder, get};
+use api::user_api::{create_user};
+use repository::mongodb_repo::MongoRepo;
 
 
 #[get("/")]
@@ -7,9 +13,17 @@ async fn hello() -> impl Responder {
 }
 
 #[actix_web::main]
-async fn main() std::io::Result<()> {
-    HttpServer::new(|| App::new().service(hello))
-        .bind(("localhost", 8080))?
-        .run()
-        .await
+async fn main() -> std::io::Result<()> {
+    let db = MongoRepo::init().await;
+    let db_data = Data::new(db);
+
+    HttpServer::new(move || {
+        App::new()
+          .app_data(db_data.clone())
+          .service(hello)
+          .service(create_user)
+    })
+   .bind(("localhost", 8080))?
+   .run()
+   .await
 }
